2ea5b4d3d67ecf7987d5a46385e98eed87d0ee7d
Mateo Arias <luismateoariascaicedo@gmail.com>
2026-09-21 23:18:03 -0500

    Multilayer neural network skeleton.

--- src/neural_network/neural_network.py
+++ src/neural_network/neural_network.py
@@ -1,9 +1,14 @@
 import numpy
 
 
-def simple_neural_network(
-    inputs,
-    weights,
-    biases,
-):
-    return numpy.dot(inputs, numpy.array(weights).T) + biases
+def single_layer_nn(inputs, weight, bias):
+    return numpy.dot(numpy.array(inputs), numpy.array(weight).T) + bias
+
+
+def multi_layer_nn(inputs, weights, biases):
+    current_input = inputs
+
+    for weight, bias in zip(weights, biases):
+        current_input = single_layer_nn(current_input, weight, bias)
+
+    return current_input
